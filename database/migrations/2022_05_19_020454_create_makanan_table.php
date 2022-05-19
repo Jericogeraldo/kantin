7aeda3b517da087e29fde887225e4f26bb1840d0
Srinesia28 <srinesiasitorus118@gmail.com>
2022-05-19 23:15:39 +0700

    migrationmakanandanenv

---
+++ database/migrations/2022_05_19_020454_create_makanan_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMakananTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('makanan', function (Blueprint $table) {
+            $table->id();
+            $table->string('namamakanan');
+            $table->string('harga');
+            $table->string('gambar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('makanan');
+    }
+}
